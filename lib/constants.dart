7ba19c4b66e7478ecba3e3b0224e5cd43676be35
dhruvpatidar359 <dhruvpatidar35@gmail.com>
2023-06-04 15:23:27 +0530

    testing

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 double ?h;
 double ?w;
 
-const String logo = 'assets/assets/images/vizola_logo.png';
-const String background = 'assets/assets/images/background.png';
+const String logo = 'assets/images/vizola_logo.png';
+const String background = 'assets/images/background.png';
 
 var morphedList = ["Subject Matter Expert","3D Animator","Vizola"];
 
